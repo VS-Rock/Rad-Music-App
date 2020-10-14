274afe16953d3ee8cc22572756841e3240934b69
Greg <gregthompson27@gmail.com>
2020-10-14 11:55:37 -0500

    (add) basic component for hotel landing page

---
+++ client/src/components/hotels/Hotels.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+const Hotels = () => {
+  const [hotels, setHotels] = [];
+  
+  return (
+    <div>
+      <p>What city are you looking to stay in?</p>
+    </div>
+  )
+}
+
+export default Hotels;
