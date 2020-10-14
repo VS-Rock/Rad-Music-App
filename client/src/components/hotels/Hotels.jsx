b42afc42e0501345c124467c5f870ff3e2c05878
Greg <gregthompson27@gmail.com>
2020-10-14 15:30:14 -0500

    (add) search button that collects text from input

--- client/src/components/hotels/Hotels.jsx
+++ client/src/components/hotels/Hotels.jsx
@@ -1,13 +1,20 @@
 import React, { useState } from 'react';
 
 const Hotels = () => {
-  const [hotels, setHotels] = [];
-  
+  const [hotels, setHotels] = useState([]);
+  const [city, setCity] = useState('');
+
+  const searchHotels = () => {
+    console.log('you are searching for hotels in ', city);
+  };
+
   return (
     <div>
       <p>What city are you looking to stay in?</p>
+      <input type="text" placeholder="City" onChange={(e) => { setCity(e.target.value); }} />
+      <button onClick={searchHotels}>Search</button>
     </div>
-  )
-}
+  );
+};
 
 export default Hotels;
