7a5c3d89744844a792f3cbc37c945e3a274fffea
Christopher Stumpe <toph.stumpe@gmail.com>
2020-10-14 21:50:16 -0500

    (update) weather router

--- server/app.js
+++ server/app.js
@@ -15,6 +15,7 @@ const { Oauth } = require('./api/oauth');
 const { Profile } = require('./api/profile');
 const { Twilio } = require('./api/twilio');
 const { Message } = require('./api/message')
+const { Weather } = require('./api/weather')
 
 const CLIENT_PATH = path.resolve(__dirname, '../client/dist');
 
@@ -33,6 +34,7 @@ app.use('/api/oauth', Oauth);
 app.use('/api/profile', Profile);
 app.use('/api/twilio', Twilio);
 app.use('/api/message', Message);
+app.use('/api/weather', Weather);
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   next();
