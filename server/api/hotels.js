351ff8fdc29ce4a447a987ab555d94812c519f0b
Greg <gregthompson27@gmail.com>
2020-10-16 16:28:45 -0500

    (add) additional amadeus API call

--- server/api/hotels.js
+++ server/api/hotels.js
@@ -16,6 +16,7 @@ Hotels.get('/', (req, res) => {
   axios(cityCodeConfig)
     .then((response) => response.data.data[0].iataCode)
     .then((cityCode) => {
+      console.log('searching from city code', cityCode);
       axios({
         method: 'get',
         url: `https://test.api.amadeus.com/v2/shopping/hotel-offers?cityCode=${cityCode}`,
@@ -24,8 +25,24 @@ Hotels.get('/', (req, res) => {
         },
       })
         .then(results => { 
-          res.send(results.data.data) 
-        })
+          console.log('initial results.data searching by cityCode:', results.data);
+          if (results.data.data.length > 0) {
+            res.send(results.data.data) 
+          } else {
+            console.log('no hotels found. doing third amadeus call now.');
+            console.log('link being sent:', results.data.meta.links.next);
+            axios({
+              method: 'get',
+              url: results.data.meta.links.next,
+              headers: { 
+                Authorization: `Bearer ${process.env.AMADEUS_TOKEN}`
+              }
+            })
+              .then(results => {
+                res.send(results.data.data);
+              });
+          }
+        });
     })
     .catch((err) => console.error(err));
 })
