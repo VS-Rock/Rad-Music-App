f95930d995dbc7e19705d2f9ebcc7b59cc5dd24a
Greg <gregthompson27@gmail.com>
2020-10-15 19:54:01 -0500

    (add) handler for axios response
    Component recieves hotel data back from axios request, and then
    component renders multiple hotel entries by mapping over data

--- client/src/components/hotels/Hotels.jsx
+++ client/src/components/hotels/Hotels.jsx
@@ -1,25 +1,29 @@
 import React, { useState } from 'react';
+import { Button } from 'react-bootstrap';
 import axios from 'axios';
+import HotelEntry from './HotelEntry';
 
 const Hotels = () => {
   const [hotels, setHotels] = useState([]);
   const [city, setCity] = useState('');
 
   const searchHotels = () => {
-    console.log('you are searching for hotels in ', city);
     axios.get('/api/hotels', { params: { city } })
       .then(results => {
-        console.log(results)
+        setHotels(results.data);
       })
       .catch(err => console.error(err));
   };
 
   return (
-    <div>
-      <p>What city are you looking to stay in?</p>
+    <>
+      <p>What city are you going to stay in?</p>
       <input type="text" placeholder="City" onChange={(e) => { setCity(e.target.value); }} />
-      <button onClick={searchHotels}>Search</button>
-    </div>
+      <Button onClick={searchHotels}>Search</Button>
+      <div id="hotel-list" >
+        {hotels.map((lodge) => <HotelEntry lodge={lodge} key={lodge.hotel.hotelId} />)}
+      </div>
+    </>
   );
 };
 
