b427ba4047d0bb1ed8d083bb3f8bfe0d40898702
Bri-b <bri.skinner02@gmail.com>
2020-10-17 12:56:08 -0500

    (add) jpeg / png file check

--- client/src/components/Messages/AddMessage.jsx
+++ client/src/components/Messages/AddMessage.jsx
@@ -2,17 +2,17 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 import {
-  Form, Button, Upload, Input,
+  Form, Button, Upload, Input, message, notification,
 } from 'antd';
 import ImgCrop from 'antd-img-crop';
 import { UploadOutlined, InboxOutlined } from '@ant-design/icons';
 
-
 export default function AddMessage({ user, showId }) {
-  const [text, setText] = useState({});
+  const [text, setText] = useState('');
   const [userId, setUserId] = useState(null);
+  const [form] = Form.useForm();
   const [fileList, setFileList] = useState([]);
-  
+
   const getUserId = () => {
     const url = `/api/messages/name/${user}`;
     axios.get(url)
@@ -22,34 +22,53 @@ export default function AddMessage({ user, showId }) {
       .catch((err) => console.error(err));
   };
 
-  const normFile = e => {
+  const normFile = (e) => { // not used
     if (Array.isArray(e)) {
       return e;
     }
-    console.log('Upload event:', e, "&&&&&&&&", e.fileList);
+    console.log('Upload event:', e, '&&&&&&&&', e.fileList);
     return e && e.fileList;
   };
-  const onFinish = values => {
+  const onFinish = (values) => {
     console.log('Received values of form: 32', values);
     setText(values.text);
     console.log('Received text of form: 33', text);
     console.log('Received fileList of form: 34', fileList);
-    console.log(`${process.env.REDIRECT}/api/messages/post`);
+    console.log('thumbnail url', fileList.map((obj) => obj.thumbUrl));
+    console.log(`${process.env.REDIRECT}api/messages/post`);
+    form.resetFields();
+    setFileList([]);
+    setText('');
   };
 
   const onChange = ({ fileList: newFileList }) => {
     setFileList(newFileList);
   };
-  const before = () => {
-    console.log(before);
-    return true;
-  }
+  const openNotificationWithIcon = (type, text) => {
+    notification[type]({
+      message: 'Upload Error',
+      description: text,
+      placement: 'bottomLeft',
+    });
+  };
+  const before = (file) => {
+    const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
+    if (!isJpgOrPng) {
+      openNotificationWithIcon('error', 'You can only upload JPG/PNG file!');
+      setFileList([]);
+    }
+    const isLt2M = file.size / 1024 / 1024 < 2;
+    if (!isLt2M) {
+      openNotificationWithIcon('error', 'Image must smaller than 2MB!');
+      setFileList([]);
+    }
+    return isJpgOrPng && isLt2M;
+  };
 
-  const onPreview = async file => {
-    console.log('OnPreview, 42');
+  const onPreview = async (file) => {
     let src = file.url;
     if (!src) {
-      src = await new Promise(resolve => {
+      src = await new Promise((resolve) => {
         const reader = new FileReader();
         reader.readAsDataURL(file.originFileObj);
         reader.onload = () => resolve(reader.result);
@@ -69,6 +88,7 @@ export default function AddMessage({ user, showId }) {
     <Form
       name="messagePost"
       onFinish={onFinish}
+      form={form}
     >
       <Form.Item name="text" label="Introduction">
         <Input.TextArea />
