164ca43f6b7ad729b67e9f9276b1625806d668f9
Greg <gregthompson27@gmail.com>
2020-10-17 15:25:34 -0500

    (refactor) api call to get new token for each call
    Previously needed to manually get new token outside app every
    30 minutes.

--- server/api/hotels.js
+++ server/api/hotels.js
@@ -1,44 +1,66 @@
 require('dotenv').config();
 const axios = require('axios');
 const { Router } = require('express');
+const qs = require('qs');
 
 const Hotels = Router();
 
+const tokenData = qs.stringify({
+  grant_type: process.env.GRANT_TYPE,
+  client_id: process.env.AMADEUS_CLIENT_ID,
+  client_secret: process.env.AMADEUS_CLIENT_SECRET,
+});
+
+const tokenConfig = {
+  method: 'post',
+  url: 'https://test.api.amadeus.com/v1/security/oauth2/token',
+  headers: { 
+    'Content-Type': 'application/x-www-form-urlencoded'
+  },
+  data: tokenData
+};
+
 Hotels.get('/', (req, res) => {
   const { city } = req.query;
-  const cityCodeConfig = {
-    method: 'get',
-    url: `https://test.api.amadeus.com/v1/reference-data/locations?subType=CITY&keyword=${city}`,
-    headers: {
-      Authorization: `Bearer ${process.env.AMADEUS_TOKEN}`
-    },
-  };
-  axios(cityCodeConfig)
-    .then((response) => response.data.data[0].iataCode)
-    .then((cityCode) => {
+  let bearerToken = '';
+  axios(tokenConfig)
+    .then((results) => {
+      bearerToken = results.data.access_token;
+    })
+    .then(() => {
       axios({
         method: 'get',
-        url: `https://test.api.amadeus.com/v2/shopping/hotel-offers?cityCode=${cityCode}`,
-        headers: { 
-          Authorization: `Bearer ${process.env.AMADEUS_TOKEN}`
-        },
+        url: `https://test.api.amadeus.com/v1/reference-data/locations?subType=CITY&keyword=${city}`,
+        headers: {
+          Authorization: `Bearer ${bearerToken}`
+        }
       })
-        .then((results) => { 
-          if (results.data.data.length > 0) {
-            res.send(results.data.data) 
-          } else {
-            axios({
-              method: 'get',
-              url: results.data.meta.links.next,
-              headers: { 
-                Authorization: `Bearer ${process.env.AMADEUS_TOKEN}`
+        .then((response) => response.data.data[0].iataCode)
+        .then((cityCode) => {
+          axios({
+            method: 'get',
+            url: `https://test.api.amadeus.com/v2/shopping/hotel-offers?cityCode=${cityCode}`,
+            headers: { 
+              Authorization: `Bearer ${bearerToken}`
+            },
+          })
+            .then((results) => { 
+              if (results.data.data.length > 0) {
+                res.send(results.data.data) 
+              } else {
+                axios({
+                  method: 'get',
+                  url: results.data.meta.links.next,
+                  headers: { 
+                    Authorization: `Bearer ${bearerToken}`
+                  }
+                })
+                  .then((results) => {
+                    res.send(results.data.data);
+                  });
               }
-            })
-              .then((results) => {
-                res.send(results.data.data);
-              });
-          }
-        });
+            });
+        })
     })
     .catch((err) => console.error(err));
 })
