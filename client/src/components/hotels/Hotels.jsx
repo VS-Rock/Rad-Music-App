e28e3951f47fa274e7c18a2b494e290675590167
Greg <gregthompson27@gmail.com>
2020-10-18 20:15:21 -0500

    (add) sorting ability for hotels
    Users can sort hotels by price (both ways) and can sort by rating,
    from high to low. Can also filter to only show 4+ star hotels.

--- client/src/components/hotels/Hotels.jsx
+++ client/src/components/hotels/Hotels.jsx
@@ -19,11 +19,38 @@ const Hotels = () => {
       .catch(err => console.error(err));
   };
 
+  const filterResults = (value) => {
+    if (value === 'lowToHigh') {
+      setShownHotels(hotels.slice().sort((a, b) => {
+        return a.offers[0].price.base - b.offers[0].price.base;
+      }));
+    } else if (value === 'highToLow') {
+      setShownHotels(hotels.slice().sort((a, b) => {
+        return b.offers[0].price.base - a.offers[0].price.base;
+      }));
+    } else if (value === 'highest-rated') {
+      setShownHotels(hotels.slice().sort((a, b) => b.hotel.rating - a.hotel.rating));
+    } else {
+      setShownHotels(hotels.filter((hotel) => hotel.hotel.rating >= 4))
+    }
+  }
+
   return (
     <>
       <p>What city are you going to stay in?</p>
       <input type="text" placeholder="City" onChange={(e) => { setCity(e.target.value); }} />
       <Button onClick={searchHotels}>Search</Button>
+      <label htmlFor="sort" >Sort by:</label>
+      <select name="sort" id="sort" onChange={(e) => { filterResults(e.target.value); }}>
+        <optgroup label="Price">
+          <option value="lowToHigh">Low to High</option>
+          <option value="highToLow">High to Low</option>
+        </optgroup>
+        <optgroup label="By Rating">
+          <option value="highest-rated">Highest Rated</option>
+          <option value="4-and-up">Only 4 Stars and Above</option>
+        </optgroup>
+      </select>
       <div id="hotel-list" >
         {shownHotels.map((lodge) => <HotelEntry lodge={lodge} key={lodge.hotel.hotelId} />)}
       </div>
